2dcc8ec2058249f0924f36542e99cf312adc2bc7
Filip Saina <fsaina@croz.net>
2019-01-03 19:43:30 +0100

    Inital project commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Faculty of Electrical Engineering and Computing
+Bioinformatics projects
+
+Replication of paper:
+Space-efficient and exact de Bruijn graph representation based on a Bloom filter ( https://almob.biomedcentral.com/articles/10.1186/1748-7188-8-22 )
+
+### Setup project:
+1. Clone this repo
+2. Download or compile Jellyfish executable ( https://github.com/gmarcais/Jellyfish ) into project root directory as 'jellyfish'
+3. Compile the project with `g++ -std=c++14 main.cpp -o DeBrujinBloom`
+
+### Data
+The E.Coli genome (data/ecoli.fasta) was taken from http://bacteria.ensembl.org/Escherichia_coli_14a/Info/Index/
+// TODO create synthetic genome sequences
---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+vector<string> read_file_in_vector(string inputPath){
+    vector<string> mers;
+
+    ifstream in(inputPath);
+
+    string line;
+    while(getline(in, line)) {
+       if (line.size() > 4) {  // TODO fix magic number
+           mers.push_back(line);
+       }
+    }
+
+    in.close();
+    return mers;
+}
+
+int main() {
+    int k = 21;
+    int minAbundance = 3;
+
+    string inputPath = "/Users/filipsaina/CLionProjects/DeBrujinBloom/data/ecoli.fasta";
+    string outputPath = "";
+
+    string workingDir = "/Users/filipsaina/CLionProjects/DeBrujinBloom/";
+    string tmpDir = workingDir + "tmp/";
+
+    string jellyfishBinPath = workingDir + "jellyfish";
+    string jellyfishTmpFilePath = "tmp.fa";
+    string defaultJellyfishOutput = "mer_counts.jf";
+
+    string command = "";
+
+    // Remove and create tmp directory
+    command = "rm -rf " + tmpDir;
+    system(command.c_str());
+
+    command = "mkdir " + tmpDir;
+    system(command.c_str());
+
+    // Count all k-mers https://github.com/gmarcais/Jellyfish/tree/master/doc
+    command = jellyfishBinPath + " count -m " + to_string(k)
+            + " -s 100M -t 4 -C -L " + to_string(minAbundance)
+            + " " + inputPath
+            + " -o " + tmpDir + defaultJellyfishOutput;
+    system(command.c_str());
+
+    // Convert binary jellyfish output to human readable format
+    command = jellyfishBinPath + " dump " + tmpDir + defaultJellyfishOutput + "_0" " > " + tmpDir + jellyfishTmpFilePath;
+    system(command.c_str());
+
+    // read the file and load only the k-mers (not their counts)
+    vector<string> mers = read_file_in_vector(tmpDir + jellyfishTmpFilePath);
+
+    // TODO remove me later
+    for (string s : mers) {
+        cout << s << endl;
+    }
+
+    return 0;
+}
