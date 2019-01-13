c8b77e6cc34ce9a4f67b8f5f35260e954df84f03
Filip Saina <fsaina@croz.net>
2019-01-13 21:49:43 +0100

    Changed default OS to Linux

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,4 @@
 mkdir ./bin
-wget https://github.com/gmarcais/Jellyfish/releases/download/v2.2.10/jellyfish-macosx -O ./bin/jellyfish
+wget https://github.com/gmarcais/Jellyfish/releases/download/v2.2.10/jellyfish-linux -O ./bin/jellyfish
 chmod +x ./bin/jellyfish
 g++ -std=c++14 main.cpp BloomFilter.cpp ExactDeBruijnGraph.cpp KmerUtil.cpp MurmurHash3.cpp measures.cpp -o ./bin/DeBrujinBloom
